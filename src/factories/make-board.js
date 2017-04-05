4166d0ec1b8af20e1d568464fe309cb30cdacddc
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-04-05 02:07:58 +0000

    patch: add board prototype to make_board

--- src/factories/make-board.js
+++ src/factories/make-board.js
@@ -1,6 +1,6 @@
 function make_board() {	
 	
-	return {
+	const board = {
 		spaces: {
 			1: null, 2: null, 3: null, 4: null, 5: null, 6: null, 7: null, 8: null, 9: null
 		},
@@ -29,6 +29,8 @@ function make_board() {
 			);
 		}
 	};
+	
+	return Object.create(board);
 }
 
 
