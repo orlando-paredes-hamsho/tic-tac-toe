cf96356572fbd50e091a216623c548571c90f1fb
paredesorlandoh <paredesorlandoh@gmail.com>
2017-04-06 14:17:39 -0400

    patch: prepare tests for the AI on any other decision

--- test/perfect-ai-test.js
+++ test/perfect-ai-test.js
@@ -28,41 +28,53 @@ describe.only('Perfect AI', () => {
 	});
 	
 	context('Making a move', () => {
-		context('When the board is empty', () => {
-			it('chooses the 5th space', () => {
-				board = make_board();
-				perfect_ai = make_ai();
-				expect(perfect_ai.move(board.get_empty_spaces())).to.equal(5);
+		
+		context('First Move', () => {
+			context('When the board is empty', () => {
+				it('chooses the 5th space', () => {
+					board = make_board();
+					perfect_ai = make_ai();
+					expect(perfect_ai.move(board.get_empty_spaces())).to.equal(5);
+				});
+			});
+
+			context('When the board has spaces occupied', () => {
+				beforeEach('Make a new board', () => {
+					board = make_board();
+					perfect_ai = make_ai();
+				});
+
+				it('chooses a different space', () => {
+					board.occupy({space: 5, marker: 'o'});
+					expect(perfect_ai.move(board.get_empty_spaces())).to.not.equal(5);
+				});
+
+				it('will pick a different corner over several games', () => {
+					board.occupy({space: 5, marker: 'o'});
+					let corner = perfect_ai.move(board.get_empty_spaces()), 
+						different_corner = perfect_ai.move(board.get_empty_spaces()), 
+						same = true;
+					while(same) {
+						if(corner === different_corner) {
+							different_corner = perfect_ai.move(board.get_empty_spaces());
+						} else {
+							same = false;
+						}
+					}
+					expect(same).to.be.false;
+				});
 			});
 		});
 		
-		context('When the board has spaces occupied', () => {
-			beforeEach('Make a new board', () => {
-				board = make_board();
-				perfect_ai = make_ai();
+		context('Moves after the first', () => {
+			context('moves with only 2 spaces taken', () => {
+				context('The oponent took a corner', () => {
+					it('');
+				});
+				context('The oponent took one of the sides', () => {
+					it('');
+				})
 			});
-			
-			it('chooses a different space', () => {
-				board.occupy({space: 5, marker: 'o'});
-				expect(perfect_ai.move(board.get_empty_spaces())).to.not.equal(5);
-			});
-			
-			it('will pick a different corner over several games', () => {
-				board.occupy({space: 5, marker: 'o'});
-				let corner = perfect_ai.move(board.get_empty_spaces()), 
-					different_corner = perfect_ai.move(board.get_empty_spaces()), 
-					same = true;
-				while(same) {
-					if(corner === different_corner) {
-						different_corner = perfect_ai.move(board.get_empty_spaces());
-					} else {
-						same = false;
-					}
-				}
-				expect(same).to.be.false;
-			});
-			
 		});
 	});
-    
 });
