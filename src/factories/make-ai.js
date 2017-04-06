f687133372540ccd34af21cdc4e93df53f138828
paredesorlandoh <paredesorlandoh@gmail.com>
2017-04-06 14:08:44 -0400

    patch: AI can make the first move

--- src/factories/make-ai.js
+++ src/factories/make-ai.js
@@ -4,8 +4,25 @@
 **/
 
 const make_ai = (type = 'perfect') => {
+	
+	const corners = [1, 3, 7, 9];
+	const center = 5;
+	
+	const get_random_corner = () => {
+		return corners[Math.round(Math.random()*3)];
+	};
+	
+	const first_move = (empty_spaces) => {
+		return (empty_spaces.indexOf(center.toString()) > -1) ? center : get_random_corner();
+	};
+	
+	const move = (empty_spaces) => {
+		if(empty_spaces.length >= 8) return first_move(empty_spaces);
+	};
+	
 	return {
-		type
+		type,
+		move
 	};
 };
 
--- test/perfect-ai-test.js
+++ test/perfect-ai-test.js
@@ -1,5 +1,8 @@
 import chai, { expect } from 'chai';
 import make_ai from '../src/factories/make-ai';
+import make_board from '../src/factories/make-board';
+
+let perfect_ai, board; 
 
 describe.only('Perfect AI', () => {
 	context('Creating the AI', () => {
@@ -21,6 +24,44 @@ describe.only('Perfect AI', () => {
 			it('defaults to perfect', () => {
 				expect(make_ai().type).to.equal('perfect');
 			});
+		});
+	});
+	
+	context('Making a move', () => {
+		context('When the board is empty', () => {
+			it('chooses the 5th space', () => {
+				board = make_board();
+				perfect_ai = make_ai();
+				expect(perfect_ai.move(board.get_empty_spaces())).to.equal(5);
+			});
+		});
+		
+		context('When the board has spaces occupied', () => {
+			beforeEach('Make a new board', () => {
+				board = make_board();
+				perfect_ai = make_ai();
+			});
+			
+			it('chooses a different space', () => {
+				board.occupy({space: 5, marker: 'o'});
+				expect(perfect_ai.move(board.get_empty_spaces())).to.not.equal(5);
+			});
+			
+			it('will pick a different corner over several games', () => {
+				board.occupy({space: 5, marker: 'o'});
+				let corner = perfect_ai.move(board.get_empty_spaces()), 
+					different_corner = perfect_ai.move(board.get_empty_spaces()), 
+					same = true;
+				while(same) {
+					if(corner === different_corner) {
+						different_corner = perfect_ai.move(board.get_empty_spaces());
+					} else {
+						same = false;
+					}
+				}
+				expect(same).to.be.false;
+			});
+			
 		});
 	});
     
