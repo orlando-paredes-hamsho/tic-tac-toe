87fd8e93bc0e4ae48157e11222ff468e045b4f90
Orlando Paredes Hamsho <paredesorlandoh@gmail.com>
2017-04-07 04:10:12 +0000

    patch: add documentation for victory utility

--- src/utility/victory.js
+++ src/utility/victory.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 
+/**
+* victory module holds victory conditions and the claim victory method.
+* @prop {Array} win_conditions
+* @method {Boolean} claim_victory
+**/
 const victory = {
+	// @public win_conditions - Array of different winning combinations
 	win_conditions: [
 		['1', '2', '3'],
 		['4', '5', '6'],
@@ -11,6 +17,11 @@ const victory = {
 		['1', '5', '9'],
 		['3', '5', '7']
 	],
+	/**
+	* @public claim_victory(claimed_spaces) check claimed spaces against win_conditions
+	* @param {Array} claimed_spaces
+	* @return {Boolean} true if the player has won
+	**/
 	claim_victory(claimed_spaces) {
 		let win = false;
 		this.win_conditions.forEach((condition) => {
